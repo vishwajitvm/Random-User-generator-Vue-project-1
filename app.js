c9a4574fa1b38118fa416e83cc4633ebcf3197ce
vishwajitvm <vishwajitmall50@gmail.com>
2022-06-16 12:10:27 +0530

    used api to genrate random users

--- app.js
+++ app.js
@@ -16,12 +16,18 @@ const app = Vue.createApp({
             const res = await fetch("https://randomuser.me/api") //api fetching
             const { results } = await res.json();
             console.log(results);
+            // this.firstName = 'preetu',
+            // this.lastname  = "verma" ,
+            // this.email  = 'preetu@gmail.com',
+            // this.gender = 'female',
+            // this.picture = 'https://randomuser.me/api/portraits/women/75.jpg'
 
-            this.firstName = 'preetu',
-            this.lastname  = "verma" ,
-            this.email  = 'preetu@gmail.com',
-            this.gender = 'female',
-            this.picture = 'https://randomuser.me/api/portraits/women/75.jpg'
+            this.firstName = results[0].name.first,
+            this.lastname  = results[0].name.last ,
+            this.email  = results[0].email,
+            this.gender = results[0].gender,
+            this.picture = results[0].picture.large
+
         }
     },
 })
